b89d49adf73f30fd988e822f752b8e61f77947ae
CHIRU VENKAT POLAVARAPU <109405680+chiru-aditya@users.noreply.github.com>
2022-11-04 23:29:52 +0530

    n=int(input()) n1=0 n2=1 s=n1+n2 while(s<=n):     n1=n2     n2=s     s=n1+n2 if abs((s-n)> abs(n2-n)):     print(n2) elif (abs(s-n)==abs(n2-n)):     a=[n2,s]     print(*a)
    else:
        ans=s
        print(ans)

--- nearest_fibonacci.py
+++ nearest_fibonacci.py
@@ -1,32 +1,17 @@
 n=int(input())
-a=0
-b=1
-c=1
-l1=[0,1]
-l2=[]
-l3=[]
-for i in range(n):
-    c=a+b
-    a=b
-    b=c
-    l1.append(c)
-if(n in l1):
-    l1.remove(n)
-for i in range(len(l1)):
-    if(l1[i]>n):
-        l3.append(l1[i])
-    else:
-        l2.append(l1[i])
-l=len(l2)
-if(n<l2[l-1]):
-    r1=l2[1-1]-n
+n1=0
+n2=1
+s=n1+n2
+while(s<=n):
+    n1=n2
+    n2=s
+    s=n1+n2
+if abs((s-n)> abs(n2-n)):
+    print(n2)
+elif (abs(s-n)==abs(n2-n)):
+    a=[n2,s]
+    print(*a)
+    
 else:
-    r1=n-l2[l-1]
-r2=l3[0]-n
-
-if(r2==r1):
-    print(l2[l-1],l3[0])
-elif(r2>r1):
-    print(l2[l-1])
-else:
-    print(l3[0])
+    ans=s
+    print(ans)
